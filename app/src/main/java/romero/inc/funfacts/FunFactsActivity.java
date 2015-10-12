356eb1e33e204a2cd3199b5505376350a052b881
algenisromero <algenisromero@Algeniss-MacBook-Pro.local>
2015-10-11 21:09:49 -0400

    FunFacts born

---
+++ app/src/main/java/romero/inc/funfacts/FunFactsActivity.java
@@ -0,0 +1,41 @@
+package romero.inc.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    private FactBook mFactBook = new FactBook();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        //Declare our view variables
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.ShowFacButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+
+                    //Refreshing the new fact
+                factLabel.setText(fact);
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+
+    }
+
+}
